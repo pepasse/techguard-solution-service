46a76957c41e57787ecfac28cbe2eb4ee87fd016
pepasse <102338652+pepasse@users.noreply.github.com>
2026-04-01 01:42:30 +0100

    Create Project model with specified fields

---
+++ backend/app/Models/Project.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'category',
+        'image_url',
+        'completion_date',
+    ];
+}
